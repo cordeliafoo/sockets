d5ccb6305d4f681dffe27c9529c489f4b66e3b8e
Shirong Foo <dee.sr.foo@gmail.com>
2017-06-14 14:25:14 +0800

    first commit

---
+++ public/sketch.js
@@ -0,0 +1,27 @@
+var socket
+
+function setup () {
+  createCanvas(windowWidth, windowHeight)
+  background(51)
+  socket = io.connect('http://localhost:8000/')
+  socket.on('mouse', newDrawing)
+}
+
+function newDrawing (data) {
+  noStroke()
+  fill(255)
+  ellipse(data.x, data.y, 10, 10)
+}
+
+function mouseDragged () {
+  var data = {
+    x: mouseX,
+    y: mouseY
+  }
+
+  socket.emit('mouse', data)
+
+  noStroke()
+  fill(255, 0, 0)
+  ellipse(mouseX, mouseY, 10, 10)
+}
