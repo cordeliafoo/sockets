ffcc62a2a32fe51218dce1f34843714d9f87db56
Shirong Foo <dee.sr.foo@gmail.com>
2017-06-14 14:29:12 +0800

    added process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(8000);
+var server = app.listen(process.env.PORT|| 8000);
 app.use(express.static('public'));
 
 var socket = require('socket.io');
